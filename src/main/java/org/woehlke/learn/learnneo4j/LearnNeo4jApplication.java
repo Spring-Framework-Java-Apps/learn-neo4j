c324c7217b0ca0d46032154ea65ba1f35062c407
a_total_another_bla <some@email.org>
2018-05-23 15:31:39 +0200

    frontend template

--- src/main/java/org/woehlke/learn/learnneo4j/LearnNeo4jApplication.java
+++ src/main/java/org/woehlke/learn/learnneo4j/LearnNeo4jApplication.java
@@ -1,10 +1,12 @@
 package org.woehlke.learn.learnneo4j;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
@@ -13,8 +15,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.woehlke.learn.learnneo4j.model.Category;
 import org.woehlke.learn.learnneo4j.model.category.CategoryRepository;
-
-
+*/
+/*
 @ComponentScan({"org.woehlke.learn.learnneo4j.model","org.woehlke.learn.learnneo4j.components"})
 @EnableJpaRepositories({"org.woehlke.learn.learnneo4j.components.db"})
 @EnableNeo4jRepositories({"org.woehlke.learn.learnneo4j.model","org.woehlke.learn.learnneo4j.components"})
@@ -22,32 +24,13 @@ import org.woehlke.learn.learnneo4j.model.category.CategoryRepository;
 @EnableWebMvc
 @EnableTransactionManagement
 @EnableSpringDataWebSupport
+*/
 @SpringBootApplication
-public class LearnNeo4jApplication implements CommandLineRunner {
+public class LearnNeo4jApplication {
 
-	@Autowired
-	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearnNeo4jApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		this.categoryRepository.deleteAll();
-		this.categoryRepository.save(new Category("www"));
-		this.categoryRepository.save(new Category("qt4"));
-		this.categoryRepository.save(new Category("qt5"));
-		// fetch all Categories
-		System.out.println("Categories found with findAll():");
-		System.out.println("--------------------------------");
-		for (Category category : this.categoryRepository.findAll()) {
-			System.out.println(category);
-		}
-		System.out.println();
-		// fetch an individual Category
-		System.out.println("Category found with findByCategory('www'):");
-		System.out.println("--------------------------------");
-		System.out.println(this.categoryRepository.findByName("www"));
-	}
 }
