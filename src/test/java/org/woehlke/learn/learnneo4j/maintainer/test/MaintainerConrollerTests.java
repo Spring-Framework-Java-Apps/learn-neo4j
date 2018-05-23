ffe8ad6e2f697cae40d131df37f2920601765787
a_total_another_bla <some@email.org>
2018-05-23 14:40:32 +0200

    frontend template

--- src/test/java/org/woehlke/learn/learnneo4j/maintainer/test/MaintainerConrollerTests.java
+++ src/test/java/org/woehlke/learn/learnneo4j/maintainer/test/MaintainerConrollerTests.java
@@ -1,4 +1,11 @@
 package org.woehlke.learn.learnneo4j.maintainer.test;
 
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class MaintainerConrollerTests {
 }
--- src/test/java/org/woehlke/learn/learnneo4j/platform/test/PlatformControllerTests.java
+++ src/test/java/org/woehlke/learn/learnneo4j/platform/test/PlatformControllerTests.java
@@ -1,4 +1,10 @@
 package org.woehlke.learn.learnneo4j.platform.test;
 
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class PlatformControllerTests {
 }
