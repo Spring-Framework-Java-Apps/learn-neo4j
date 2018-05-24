09c5b4cf8fcd6e0ed1327a9c74b733b9eaa9814b
a_total_another_bla <some@email.org>
2018-05-24 19:55:23 +0200

    frontend template

--- bin/run.sh
+++ bin/run.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-export GRAPH_DB_GOOGLE_APS_API_KEY=AIzaSyCMWLD6SK1QY3f-94mDS6x7u_jxwYASgqY
-export GRAPH_DB_GOOGLE_ANALYTICS_ID=UA-17174370-11
-export GRAPH_DB_GOOGLE_WEBMASTER_TOOLS_ID=3eksTC3Rvao6fLvSUwoRECBwd9Gdg4F51IhOE3DD-T8
-export GRAPH_DB_MICROSOFT_WEBMASTER_TOOLS_ID=52F1EDC0244BCDCF5C95E402E9E3E49B
-export GRAPH_DB_FACEBOOK_APP_ID=1721279777913278
-export GRAPH_DB_TWITTER_CARD_SITE=@port80guru
-export GRAPH_DB_TWITTER_CARD_CREATOR=@port80guru
+export DATABASE_URL='jdbc:postgresql://localhost:5432/learn-neo4j?user=jdbc&password=jdbcpwd&schema=public'
 
-../mvnw clean spring-boot:run
+source config.sh
+
+cd ..
+
+./mvnw clean spring-boot:run
