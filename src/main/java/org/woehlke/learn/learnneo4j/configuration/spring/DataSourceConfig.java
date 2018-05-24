59e5788127d6bad709031e2b88915bf3a45fef90
phasenraum2010 <Recoil89>
2018-05-24 20:15:25 +0200

    changed propeterties

--- src/main/java/org/woehlke/learn/learnneo4j/configuration/spring/DataSourceConfig.java
+++ src/main/java/org/woehlke/learn/learnneo4j/configuration/spring/DataSourceConfig.java
@@ -7,9 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 import org.springframework.session.jdbc.config.annotation.web.http.JdbcHttpSessionConfiguration;
 import org.springframework.transaction.PlatformTransactionManager;
 
@@ -20,8 +18,6 @@ import javax.sql.DataSource;
 @EnableJpaRepositories({
     "org.woehlke.learn.learnneo4j.model.orm"
 })
-@EnableJdbcHttpSession
-@EnableSpringDataWebSupport
 public class DataSourceConfig extends JdbcHttpSessionConfiguration {
 
     @Bean
